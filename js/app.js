026a4ce8693d9f442a565515a9a75f3e9bd88b38
TeeSo di Carlo Oldani <37111902+teeso@users.noreply.github.com>
2018-10-03 21:21:47 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
-//Document is the DOM can be accessed in the console with document.window.
-// Tree is from the top, html, body, p etc.
 
-//Problem: User interaction does not provide the correct results.
-//Solution: Add interactivity so the user can manage daily tasks.
-//Break things down into smaller steps and take each step at a time.
+/*
 
+'use strict';
+  var count = 0;
+document.addEventListener('keypress', (event) => {
+  const keyName = event.key;
+  
+    if (event.key === "1") {
+                count++;
+            }
+  else {  
+        count--;  
+    }
+  
+  alert('keypress event\n\n' + 'key: ' + keyName + ' ' + count + ' ' + document.lastModified);
+});
+
+*/
 
 //Event handling, uder interaction is what starts the code execution.
 
