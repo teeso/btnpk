4b3df73be5798c12bf350a006ab592381ebabd83
TeeSo di Carlo Oldani <37111902+teeso@users.noreply.github.com>
2018-10-04 08:31:16 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -155,7 +155,7 @@ var ajaxRequest=function(){
 //Set the click handler to the addTask function.
 addButton.onclick=addTask;
 addButton.addEventListener("click",addTask);
-addButton.addEventListener("click",ajaxRequest);
+//addButton.addEventListener("click",ajaxRequest);
 
 
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
